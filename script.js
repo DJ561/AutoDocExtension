66d418240391a0f8d3cfd953995a70178c52c026
DJ <138820923+DJ561@users.noreply.github.com>
2024-05-01 21:20:20 -0700

    update

--- script.js
+++ script.js
@@ -4,13 +4,26 @@ document.addEventListener("DOMContentLoaded", function() {
     const civBtn = document.getElementById("Civ");
     const settingsBtn = document.getElementById("Settings");
 
+    var url = "https://script.google.com/macros/s/AKfycbzUI1wJNw9sYF9wxPYvWbom8WVf8oxAgD8CEeCt7wk/dev"
+
     if (genBtn) {
         genBtn.addEventListener("click", function () {
-            fetch("https://script.google.com/macros/s/AKfycbzs7-wkULZVs3QqzGf9ZDXnzgXVbifISA9ywenFwuzx/dev")
-                .then(response => response.json())
-                .then(data => console.log(data))
-                .catch(error => console.error(error));
+            console.log("Genre button clicked");
+    
+            var xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.setRequestHeader("data", "?subject=genre&name=Testname");
+
+            xhr.onload = function () {
+                console.log(xhr.status);
+                console.log(xhr.responseText);
+                //chrome.tabs.create({ url: xhr.responseText });
+                }
+            
+            xhr.send();
         });
+
     }
 
     if (questBtn) {
